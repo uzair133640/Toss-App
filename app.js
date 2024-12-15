d0b1143bf8d2f97cdf5d20cf0c1baa118b8667f5
uzair133640 <uchihakakashi133640@gmail.com>
2024-12-15 18:44:54 +0500

    done

---
+++ app.js
@@ -0,0 +1,29 @@
+var img=document.querySelector(".img")
+var h=document.querySelector(".h")
+var t=document.querySelector(".t")
+var heads=0
+var tails=0
+
+
+function flip(){
+    var num=Math.floor(Math.random()*2)
+    if(num===0){
+        img.innerHTML=`<img class="heads_img" src="/img/heads.jpeg">`
+        heads++
+        h.innerHTML=`Heads: ${heads}`
+    }
+    else {
+        img.innerHTML=`<img class="tails_img" src="/img/tails.jpeg" >`
+        tails++
+        t.innerHTML=`Tails: ${tails}`
+    }
+
+}
+
+function reset(){
+    heads=0
+    tails=0
+    h.innerHTML=`Heads: 0 `
+    t.innerHTML="Tails: 0 "
+    img.innerHTML=`<img class="toss_img" src="/img/toss.png"></img>`
+}
